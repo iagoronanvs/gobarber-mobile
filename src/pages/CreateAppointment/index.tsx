423d6b188f42bfaf97c090fdbd6212bd9dd44f31
Iago Ronan <iago.santos@privatecode.com.br>
2021-07-30 00:12:28 -0300

    estrutura da criação

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
+
+const avatar_url = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png';
 
 const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+  const { user } = useAuth();
+  const {goBack} = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
 
   return (
-    <View />
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url ?? avatar_url }} />
+      </Header>
+    </Container>
   );
 };
 
